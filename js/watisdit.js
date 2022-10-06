9a563ee7c4fb62ff718b33a62af0843730059e40
bram de leeuw <bramdeleeuw@gmail.com>
2022-10-06 15:04:01 +0200

    responsive watisdit

--- js/watisdit.js
+++ js/watisdit.js
@@ -3,7 +3,10 @@
 		$('.webshoplocatie-banner .wl-whatsthis').click(function(e){
 			e.preventDefault();
 			let moseLeft = e.clientX - 36;
-			$('.wl-explanation').css({left: moseLeft +'px'})
+			if($(window).width() <= 768){
+				// only on desktop
+				$('.wl-explanation').css({left: moseLeft +'px'});
+			}
 			$('.webshoplocatie-banner .wl-explanation').toggleClass('show');
 		});
 		$('.webshoplocatie-banner .wl-close').click(function(e){
