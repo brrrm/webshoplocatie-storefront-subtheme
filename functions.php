0e70d301e0f790c5e561616ce55313b118d66d74
Cecile Enzler <cecile.enzler@gmail.com>
2023-03-13 11:43:53 +0100

    product placement home

--- functions.php
+++ functions.php
@@ -26,10 +26,13 @@ function woo_system_api( $controllers ) {
 	return $controllers;
 }
 
+remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
 add_action('woocommerce_after_shop_loop_item_title', 'webshoplocatie_woocommerce_after_shop_loop_item_title', 1);
 function webshoplocatie_woocommerce_after_shop_loop_item_title(){
+	woocommerce_template_loop_product_link_close();
+	woocommerce_template_loop_product_title();
 	echo '<ul class="product-cats">' . wc_get_product_category_list( get_the_id(), '</li><li>', '<li>', '</li>' ) . '</ul>';
 	wc_get_template( 'single-product/short-description.php' );
-	//remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 	remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 6 );
 }
